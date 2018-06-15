553d86d050c9cca36e478133fef26f02bbd2b427
Ben Douadia <simo2332001@yahoo.fr>
2018-06-15 03:13:31 +0200

    jj

--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -6,63 +6,17 @@ formatter.feature({
   "id": "google-search",
   "keyword": "Feature"
 });
-formatter.before({
-  "duration": 5481379954,
-  "status": "passed"
-});
-formatter.background({
-  "line": 3,
-  "name": "",
-  "description": "",
-  "type": "background",
-  "keyword": "Background"
-});
-formatter.step({
-  "line": 4,
-  "name": "I open a browser",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 5,
-  "name": "I navigate to google page",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "GoogleSearch.i_open_a_browser()"
-});
-formatter.result({
-  "duration": 3232338633,
-  "status": "passed"
-});
-formatter.match({
-  "location": "GoogleSearch.i_navigate_to_google_page()"
-});
-formatter.result({
-  "duration": 2694187060,
-  "status": "passed"
-});
-formatter.scenario({
+formatter.scenarioOutline({
   "line": 7,
   "name": "Validate google search text field",
   "description": "",
   "id": "google-search;validate-google-search-text-field",
-  "type": "scenario",
-  "keyword": "Scenario"
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
 });
 formatter.step({
   "line": 8,
   "name": "I validate the search tesxt field",
   "keyword": "Then "
-});
-formatter.match({
-  "location": "GoogleSearch.i_validate_the_search_tesxt_field()"
-});
-formatter.result({
-  "duration": 1309214193,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 841802860,
-  "status": "passed"
 });
 });
