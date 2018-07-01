c39cb40b03c2fa383bc40990996cd242089375a3
Ben Douadia <simo2332001@yahoo.fr>
2018-07-01 09:38:16 +0200

    ll

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -8,10 +8,9 @@ import cucumber.api.junit.Cucumber;
 //Uncomment @RunWith if you are using Junit to run Test 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features = { "src//test//java//features" }, glue = { "stepdefinitions", "utility" }, plugin = {
-		"pretty", "html:target/cucumber", "json:target/cucumber/cucumber.json",
-		"pretty:target/cucumber/cucumber-pretty.json", "usage:target/cucumber/cucumber-usage.json",
-		"junit:target/cucumber/cucumber-result.xml" })
+@CucumberOptions(features = { "src/test/java/features" }, glue = { "stepdefinitions", "utility" }, plugin = { "pretty",
+		"html:target/cucumber", "json:target/cucumber/cucumber.json", "pretty:target/cucumber/cucumber-pretty.json",
+		"usage:target/cucumber/cucumber-usage.json", "junit:target/cucumber/cucumber-result.xml" })
 
 public class RunTest {
 
